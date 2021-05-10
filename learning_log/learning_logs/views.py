7e933b1d94d6a526591a3d1a4f05fad4a1c1dc6b
olya <olya@ivanovs.info>
2021-05-10 12:25:40 +1200

    Mapping URLs and new pages.

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Topic
 
-# Create your views here.
+
+def index(request):
+    """The home page for Learning Logs."""
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show all entries for the topic."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date-added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
