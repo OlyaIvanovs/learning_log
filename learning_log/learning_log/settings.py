a09d1d39dc00693b648c3fa4e389cefa6e78b102
olya <olya@ivanovs.info>
2021-05-11 21:29:07 +1200

    Styling home page.

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party apps
+    'bootstrap4',
+
     # my_apps
     'learning_logs',
     'users',
