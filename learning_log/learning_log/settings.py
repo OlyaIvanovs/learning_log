a54a1fc57a933ce6eb162526e240e7a03966115d
olya <olya@ivanovs.info>
2021-05-12 11:58:18 +1200

    Before pushing to Heroku.

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # My settings
 LOGIN_URL = 'users:login'
+
+# Heroku settings
+django_heroku.settings(locals())
